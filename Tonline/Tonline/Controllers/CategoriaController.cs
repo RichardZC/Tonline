8ba3be3259fe9791cbd1a9d05452d9982098c9e0
RichardLaptop <r_zapatac@hotmail.com>
2016-02-01 15:54:49 -0500

    Cambios BD

---
+++ Tonline/Tonline/Controllers/CategoriaController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BE;
+using BL;
+
+namespace Tonline.Controllers
+{
+    public class CategoriaController : Controller
+    {
+       
+
+        // GET: Categoria
+        public ActionResult Index()
+        {
+            return View(CategoriaBl.Listar());
+        }
+
+        // GET: Categoria/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            categoria categoria = CategoriaBl.Obtener(id.Value);
+            if (categoria == null)
+            {
+                return HttpNotFound();
+            }
+            return View(categoria);
+        }
+
+        // GET: Categoria/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Categoria/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "CategoriaId,Denominacion")] categoria categoria)
+        {
+            if (ModelState.IsValid)
+            {
+                CategoriaBl.Crear(categoria);
+                return RedirectToAction("Index");
+            }
+
+            return View(categoria);
+        }
+
+        // GET: Categoria/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            categoria categoria = CategoriaBl.Obtener(id.Value);
+            if (categoria == null)
+            {
+                return HttpNotFound();
+            }
+            return View(categoria);
+        }
+
+        // POST: Categoria/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "CategoriaId,Denominacion")] categoria categoria)
+        {
+            if (ModelState.IsValid)
+            {
+                CategoriaBl.Actualizar(categoria);
+                return RedirectToAction("Index");
+            }
+            return View(categoria);
+        }
+
+        // GET: Categoria/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            categoria categoria = CategoriaBl.Obtener(id.Value);
+            if (categoria == null)
+            {
+                return HttpNotFound();
+            }
+            return View(categoria);
+        }
+
+        // POST: Categoria/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            CategoriaBl.Eliminar(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
