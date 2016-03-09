f26787f68090b201508b0b5ac3a85d1475613bf9
RichardLaptop <r_zapatac@hotmail.com>
2016-03-09 15:36:31 -0500

    sesion

---
+++ Tonline/Tonline/Models/sesionBE.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tonline.Models
+{
+    public class sesionBE
+    {
+        public string user { get; set; }
+        public string p { get; set; }
+        public bool sesion { get; set; }
+    }
+}
