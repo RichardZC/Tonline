83cc8ad43c90eaa311f9eddfd530773307ce4d96
rahegoce <razuhebe91@gmail.com>
2016-03-21 16:53:21 -0500

    cambios carrito

--- Tonline/Tonline/Controllers/DetalleController.cs
+++ Tonline/Tonline/Controllers/DetalleController.cs
@@ -3,16 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using BL;
+using Tonline.Models;
 namespace Tonline.Controllers
 {
 
     public class DetalleController : Controller
     {
         // GET: Detalle
-        public ActionResult Index()
+        public ActionResult Index(int id=0)
         {
+            ViewBag.ArticuloId = id;
             return View();
+        }
+
+        public JsonResult ListarCompra()
+        {
+            if (Session["compras"] == null)
+                return Json(new List<Producto>(), JsonRequestBehavior.AllowGet);
+
+            var compra = (List<Producto>)Session["compras"];
+            return Json(compra, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult ListarArticuloPrincipal()
+        {
+            return Json(ArticuloBl.ListarProducto(), JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult ObtenerArticulo(int pArticuloId)
+        {
+            return Json(ArticuloBl.Obtener(pArticuloId), JsonRequestBehavior.AllowGet);
         }
     }
 }
