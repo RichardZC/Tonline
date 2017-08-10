0b1f525b696c5c9d8f8c5b5df104457c38e75097
litzana20015 <litzana_315@hotmail.com>
2017-08-10 06:53:48 -0400

    trabajo tonline

--- Tonline/Tonline/Controllers/ArticuloController.cs
+++ Tonline/Tonline/Controllers/ArticuloController.cs
@@ -5,11 +5,14 @@ using System.Web;
 using System.Web.Mvc;
 using BE;
 using BL;
+using System.IO;
 
 namespace Tonline.Controllers
 {
     public class ArticuloController : Controller
     {
+        private object db;
+
         // GET: Articulo
         public ActionResult Index()
         {
@@ -33,6 +36,30 @@ namespace Tonline.Controllers
         {
             return Json(ArticuloBl.Listar(x => x.Denominacion.Contains(pClave)).Take(15).ToList(), JsonRequestBehavior.AllowGet);
         }
+        [HttpPost]
+        public JsonResult MantenerCaracteristica(caracteristica m)
+        {
+            if (m.CaracteristicaId == 0)
+                CaracteristicaBl.Crear(m);
+            else
+                CaracteristicaBl.Actualizar(m);
+            return Json(m, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult ListarCaracteristica(int ArticuloId)
+        {
+            return Json(CaracteristicaBl.Listar(), JsonRequestBehavior.AllowGet);
+
+        }
+
+
+        public JsonResult EliminarCaracteristica(int pid)
+        {
+            CaracteristicaBl.Eliminar(pid);
+            return Json(true, JsonRequestBehavior.AllowGet);
+        }
+      
+
 
     }
 }
---
+++ Tonline/Tonline/Models/ResponseModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Tonline.Models
+{
+    public class ResponseModel : Controller
+    {
+        // GET: ResponseModel
+        public bool respuesta { get; set; }
+        public string error { get; set; }
+    }
+}
